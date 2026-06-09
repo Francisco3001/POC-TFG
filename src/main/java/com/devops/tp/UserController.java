705030183a9b7a04bb3ae5eb1182182a6b69a666
Fran Noceda <francisconoceda3001@gmail.com>
2026-06-09 18:55:32 -0300

    vul

--- src/main/java/com/devops/tp/UserController.java
+++ src/main/java/com/devops/tp/UserController.java
@@ -25,6 +25,26 @@ public class UserController {
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
     }
+    
+    @GetMapping("/search")
+    public String searchUser(@RequestParam String username) {
+        try (Connection conn = dataSource.getConnection();
+             Statement stmt = conn.createStatement()) {
+
+            String query = "SELECT * FROM users WHERE username = '" + username + "'";
+            ResultSet rs = stmt.executeQuery(query);
+
+            StringBuilder result = new StringBuilder();
+            while (rs.next()) {
+                result.append(rs.getString("username")).append("\n");
+            }
+
+            return result.toString();
+
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
 
     @GetMapping
     public List<User> getUsers() {

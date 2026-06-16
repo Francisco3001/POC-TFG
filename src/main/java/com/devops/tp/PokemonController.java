8499988d8c425151d51905f8c2f8eb7586381d28
Fran Noceda <francisconoceda3001@gmail.com>
2026-06-15 21:10:13 -0300

    hola xd

--- src/main/java/com/devops/tp/PokemonController.java
+++ src/main/java/com/devops/tp/PokemonController.java
@@ -17,19 +17,5 @@ public class PokemonController {
         String url = "https://pokeapi.co/api/v2/pokemon/" + name;
         return restTemplate.getForObject(url, String.class);
     }
-    @GetMapping("/admin/report")
-    public ResponseEntity<String> generateReport(
-            @RequestParam String filename,
-            @RequestParam String userId,
-            @RequestParam String template) {
-
-
-        String adminToken = "eyJhbGciOiJIUzI1NiJ9.admin.supersecret123";
-        String dbPassword = "root1234";
-
-        String externalUrl = template; 
-        String externalData = restTemplate.getForObject(externalUrl, String.class);
-
-        return ResponseEntity.ok("Done: " + externalData + results.toString());
-    }
+    //hola!
 }

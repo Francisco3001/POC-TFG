38883a6b41163f48793e3d91a05763997c651a07
Fran Noceda <francisconoceda3001@gmail.com>
2026-07-16 15:47:13 -0300

    POC

--- src/main/java/com/devops/tp/UserController.java
+++ src/main/java/com/devops/tp/UserController.java
@@ -30,6 +30,34 @@ public class UserController {
     public List<User> getUsers() {
         return userRepository.findAll();
     }
-    //hola
+
+    @GetMapping("/search")
+    public List<User> search(@RequestParam String username) {
+
+        String sql = "SELECT * FROM users WHERE username = '" + username + "'";
+
+        return entityManager
+                .createNativeQuery(sql, User.class)
+                .getResultList();
+    }
+    @GetMapping("/file")
+    public String readFile(@RequestParam String filename) throws IOException {
+
+        Path path = Paths.get("uploads/" + filename);
+
+        return Files.readString(path);
+    }
+    
+    @GetMapping("/config")
+    public Map<String, String> config() {
+
+        Map<String, String> config = new HashMap<>();
+
+        config.put("dbUser", "postgres");
+        config.put("dbPassword", "postgres123");
+        config.put("jwtSecret", "my-super-secret-key");
+
+        return config;
+    }
 
 }
